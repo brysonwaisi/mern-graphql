c4e826c2ab6ab350dc719f8099144020d847743d
brysonwaisi <brysonnyamwange@gmail.com>
2023-06-26 22:02:24 +0300

    Setup Client and Apollo Ccahe

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Clients from "./components/Clients";
+
+//for the cache data might be lost error in apollo
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache,
+});
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Router>
+          <Header />
+          <div className="container">
+            <Clients />
+          </div>
+        </Router>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
@@ -10,6 +11,8 @@ const app = express();
 
 // connect DB
 connectDB();
+
+app.use(cors());
 
 app.use(
   "/graphql",
